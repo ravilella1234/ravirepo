06cb7521eeae3175ac12f70f1e27f7eabd316e0b
maha <maha@gmail.com>
2020-07-19 19:47:40 +0530

    asadada

--- FirstProject/src/com/icici/loans/homeloans/DemoClass.java
+++ FirstProject/src/com/icici/loans/homeloans/DemoClass.java
@@ -4,7 +4,7 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("iam firstline....");
+		System.out.println("iam firstline...."); 
 	}
 
 }
