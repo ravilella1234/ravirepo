c588e28a95d41f4fd93617db071a55c40459eb4c
maha <maha@gmail.com>
2020-07-19 19:28:43 +0530

    added project4 into ravirepo

---
+++ Project4/src/pack4/Class4.java
@@ -0,0 +1,15 @@
+package pack4;
+
+public class Class4 
+{
+	int a=100,b=200,c;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("iam Class4");
+		System.out.println("added second line ..");
+		System.out.println("added Third line ..");
+		System.out.println("added fourth line .. "); 
+	}
+
+}
