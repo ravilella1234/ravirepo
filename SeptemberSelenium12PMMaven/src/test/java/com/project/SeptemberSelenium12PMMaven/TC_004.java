e52bff791b533573dc22eade419b38be3a3ea417
maha <maha@gmail.com>
2020-09-22 12:58:31 +0530

    TestNG Day-1

---
+++ SeptemberSelenium12PMMaven/src/test/java/com/project/SeptemberSelenium12PMMaven/TC_004.java
@@ -0,0 +1,12 @@
+package com.project.SeptemberSelenium12PMMaven;
+
+import org.testng.annotations.Test;
+
+public class TC_004 
+{
+  @Test
+  public void f() 
+  {
+	  System.out.println("f test");
+  }
+}
